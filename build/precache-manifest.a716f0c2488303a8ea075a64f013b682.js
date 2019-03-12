8f554702433719d715bdbcb47c10b0581408b56a
Byron Prescott <prescott.byron@gmail.com>
2019-03-12 12:21:08 +0000

    react TICTAC game

---
+++ build/precache-manifest.a716f0c2488303a8ea075a64f013b682.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "c2bc47bf3374b5abf30c",
+    "url": "/static/js/main.c2bc47bf.chunk.js"
+  },
+  {
+    "revision": "0bd08c5f063516b7ff94",
+    "url": "/static/js/2.0bd08c5f.chunk.js"
+  },
+  {
+    "revision": "c2bc47bf3374b5abf30c",
+    "url": "/static/css/main.3d7d8666.chunk.css"
+  },
+  {
+    "revision": "20f804dc58e608c16c33c0b62aa7afa2",
+    "url": "/index.html"
+  }
+];
